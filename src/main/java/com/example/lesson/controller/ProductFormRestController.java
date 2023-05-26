05ab4012fab0fa9018a9c72bd54ab2da943d1e41
OyafusoNatsuki <oyafuso.natsuki.2023.kensyu@gmail.com>
2023-05-26 12:35:52 +0900

    更新処理まで(仮)

--- src/main/java/com/example/lesson/controller/ProductFormRestController.java
+++ src/main/java/com/example/lesson/controller/ProductFormRestController.java
@@ -4,9 +4,7 @@ import com.example.lesson.entity.ProductRecord;
 import com.example.lesson.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,27 +14,29 @@ public class ProductFormRestController {
     @Autowired
     ProductService productService;
 
-    @GetMapping("product2-list")
+    @GetMapping("/product2-list")
     public List<ProductRecord> list() {
         return productService.findAll();
     }
 
-    @GetMapping("product-add")
-    public void add(@RequestParam(name = "name")String name,
+    @GetMapping("/product2-add")
+    public void add2(@RequestParam(name = "name")String name,
                              @RequestParam(name = "price")Integer price) {
         productService.insert(new ProductRecord(null, name, price));
     }
 
-    @GetMapping("product-detail")
-    public ProductRecord detail(@RequestParam(name = "id")Integer id) {
-        System.out.println("SECCCC");
+    @GetMapping("/product2-detail-view")
+    public ProductRecord detailView(@RequestParam(name = "id")Integer id) {
         ProductRecord product = productService.findById(id);
         return product;
     }
 
-//    @GetMapping("product2-add")
-//    public ProductRecord add() {
-//
-//    }
+//    @GetMapping("product2-update")
+    @PutMapping("/product2-update")
+    public void update2(@RequestParam(name = "id")Integer id,
+                        @RequestParam(name = "name")String name,
+                        @RequestParam(name = "price")Integer price) {
+        productService.update(new ProductRecord(id, name, price));
+    }
 
 }
